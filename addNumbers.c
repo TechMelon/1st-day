17dc55d8995d538a1dcf463c0f0f389ebad974bc
sneha <snehabhardwaj035@gmail.com>
2024-06-02 10:15:34 +0530

    adding numbers

---
+++ addNumbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter number A :\n");
+    scanf("%d",&a);
+    printf("Enter number B :\n");
+    scanf("%d",&b);
+    printf("Sum of A and B is %d",a+b);
+    return 0;
+}
