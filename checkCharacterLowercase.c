06dfd46231534558027b4d428a9b1abb3cdd0245
sneha <snehabhardwaj035@gmail.com>
2024-06-04 09:59:47 +0530

    checking entered character is lowercase or not.

---
+++ checkCharacterLowercase.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char n;
+    printf("Enter the character : \n");
+    scanf("%c",&n);
+    if (n>='a' && n<='z')
+    {
+        printf("Character is lowercase.");
+    }
+    else
+    {
+        printf("Character is uppercase.");
+    }
+}
