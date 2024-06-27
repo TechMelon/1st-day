8416b32507bf9daf5e1914e60d52e7bfc4399ca4
sneha <snehabhardwaj035@gmail.com>
2024-06-27 13:10:02 +0530

    find the sum of the digits of a given number

--- Loops/sumDigits.c
+++ Loops/sumDigits.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
 int main(){
-    
+    int n , sum=0;
+    scanf("%d",&n);
+    for (int i =n; i != 0; i/=10)
+    {
+        sum = sum+i%10;
+    }
+    printf("%d",sum);
+    return 0;
 }
