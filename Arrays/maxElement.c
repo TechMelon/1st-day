f8e54440a46444b6223d386e3b13e8fcfda8fe85
sneha <snehabhardwaj035@gmail.com>
2024-07-01 16:11:45 +0530

    array-find max element of array

---
+++ Arrays/maxElement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a[5] = {5,9,6,7,3};
+    int max = a[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (a[i]>max)
+        {
+            max = a[i];
+            printf("%d",max);
+        }
+    }
+    return 0;
+}
